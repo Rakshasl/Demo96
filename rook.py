51e83f69dd5e073c4b2c8dd3e65ea70aadda6cde
Rakshasl <49223138+Rakshasl@users.noreply.github.com>
2019-05-28 13:16:00 +0530

    Add files via upload

---
+++ rook.py
@@ -0,0 +1,15 @@
+from pieces.piece import Piece
+
+
+class Rook(Piece):
+
+    alliance = None
+    position = None
+
+    def __init__(self,alliance,position):
+        self.alliance =alliance
+        self.position = position
+
+    def toString(self):
+        return "R" if self.alliance == "Black" else "r"
+
